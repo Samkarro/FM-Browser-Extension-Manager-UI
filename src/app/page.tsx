f0725425c11d6555c535c9bda18222d58d9dade7
Luka Gogichaishvili <luka595664@gmail.com>
2025-11-17 20:55:14 +0400

    feat: element/file structure
    Added most required elements of the page. Modularization pending.
    
    Changed file structure for ease and to reflect JSON data usage.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,50 @@
+import data from "./common/data.json";
+
 export default function Home() {
+  const stringified = JSON.stringify(data);
+  const extensions: {
+    logo: string;
+    name: string;
+    description: string;
+    isActive: boolean;
+  }[] = JSON.parse(stringified);
   return (
-    <div>
-      <main></main>
+    <div className="content-container">
+      <header>
+        <svg href="./assets/images/logo.svg" />
+        <div className="theme-button">
+          <svg href="./assets/images/icon-moon.svg" />
+        </div>
+      </header>
+      {/* TODO: make this whole thing a separate tsx */}
+      <main>
+        <div className="extension-sorter-container">
+          <h1>Extensions List</h1>
+
+          <div className="extension-sorter">
+            {/* TODO: implement buttons */}
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+        <div className="extensions-container">
+          {extensions &&
+            extensions.map((extension) => {
+              return (
+                <div className="extension" key={extension.name}>
+                  <div className="extension-info-container">
+                    {extension.name}
+                  </div>
+                  <div className="extension-interactions-container">
+                    <div className="remove-button">Remove</div>
+                    <div className="toggle"></div>
+                  </div>
+                </div>
+              );
+            })}
+        </div>
+      </main>
     </div>
   );
 }
